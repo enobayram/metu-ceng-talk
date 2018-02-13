30c2bd603d4b0471c8a726fbaca1372d092f236f
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2018-02-13 11:01:04 +0300

    Added a deposit function

--- Main.hs
+++ Main.hs
@@ -16,12 +16,17 @@ main = do
   putStrLn $ show m1
   let (m2, r2) = createAccount (AccountId 2) m1
   putStrLn $ show r2 ++ ": " ++ show m2
-  let (m3, r3) = createAccount (AccountId 3) m2
+  let (m3, r3) = deposit (AccountId 2) (Dollar 3.2) m2
   putStrLn $ show r3 ++ ": " ++ show m3
-  let (m4, r4) = createAccount (AccountId 2) m3
+  let (m4, r4) = deposit (AccountId 1) (Dollar 1) m3
   putStrLn $ show r4 ++ ": " ++ show m4
 
 data CreateResult = Created | AlreadyExists deriving Show
+
+deposit :: AccountId -> Dollar -> AccountMap -> (AccountMap, Maybe Dollar)
+deposit accId amt m = case accId `lookup` m of 
+  Just old -> (adjust (+amt) accId m, Just $ old + amt)
+  Nothing -> (m, Nothing)
 
 createAccount :: AccountId -> AccountMap -> (AccountMap, CreateResult)
 createAccount accId m = 
